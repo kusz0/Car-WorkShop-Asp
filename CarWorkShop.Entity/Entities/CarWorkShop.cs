9a11c0fa2e5b2a2965d4a9c2c764385d836b2521
Kacper Kuszek <kuszekkacper@gmail.com>
2026-01-05 22:19:26 +0100

    Data model added

---
+++ CarWorkShop.Entity/Entities/CarWorkShop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkShop.Entity.Entities
+{
+    public class CarWorkShop
+    {
+        public required int id {  get; set; }
+        public string Name { get; set; } = default!;
+        public string? Description { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public CarWorkShopContactDetails ContactDetails { get; set; } = default!;
+        public string EncodedName { get; private set; } = default!;
+
+        public void EncodeName() => EncodedName = Name.ToLower().Replace(" ", "-");
+
+
+
+
+    }
+}
