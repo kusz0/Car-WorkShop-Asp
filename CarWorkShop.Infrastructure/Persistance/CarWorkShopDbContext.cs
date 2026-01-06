b6fcd0087b0f2f2ceeacd9b858127e0807a91165
Kacper Kuszek <kuszekkacper@gmail.com>
2026-01-06 20:37:30 +0100

    Entity framework added

---
+++ CarWorkShop.Infrastructure/Persistance/CarWorkShopDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkShop.Infrastructure.Persistance
+{
+    public class CarWorkShopDbContext : DbContext
+    {
+        public DbSet<Entity.Entities.CarWorkShop> CarWorkShops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
