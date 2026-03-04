7cfc4c7370e3d8d25da82e3b3d4b6730a2f6b32f
Kacper Kuszek <kuszekkacper@gmail.com>
2026-03-04 23:59:14 +0100

    seed

---
+++ CarWorkShop.Infrastructure/Seeders/CarWorkShopSeeder.cs
@@ -0,0 +1,20 @@
+﻿using CarWorkShop.Infrastructure.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkShop.Infrastructure.Seeders
+{
+    internal class CarWorkShopSeeder
+    {
+        private readonly CarWorkShopDbContext _dbContext;
+        public CarWorkShopSeeder(CarWorkShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
