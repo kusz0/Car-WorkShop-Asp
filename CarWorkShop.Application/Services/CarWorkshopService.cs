ae4c3d38c26f47d2799268b6f3d9b5357e9228ce
Kacper Kuszek <kuszekkacper@gmail.com>
2026-03-09 21:08:31 +0100

    added carworkshop controllers

---
+++ CarWorkShop.Application/Services/CarWorkshopService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkShop.Application.Services
+{
+    internal class CarWorkshopService
+    {
+        public async Task Create() 
+        {
+            await return Task.CompletedTask;
+        }
+    }
+}
---
+++ CarWorkShop.MVC/Controllers/CarWorkShopController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace CarWorkShop.MVC.Controllers
+{
+    public class CarWorkShopController : Controller
+    {
+        public CarWorkShopController()
+        {
+            
+        }
+        [HttpPost]
+        public IActionResult Create(Entity.Entities.CarWorkShop carWorkshop) 
+        {
+
+        }
+
+    }
+}
